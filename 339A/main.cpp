6ce2a3f6867ddc234fe5c0936c85082c8137f1e9
Łukasz Synowiec <lukaszsynowiec15@gmail.com>
2020-06-20 14:26:26 +0200

    Clean up 339A

--- 339A/main.cpp
+++ 339A/main.cpp
@@ -1,35 +1,31 @@
-#include<iostream>
-#include<algorithm>
-#include<vector>
-#include<string>
-#include<cstdlib>
-using namespace std;
+#include <algorithm>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <vector>
 
+using namespace std;
 
 int main()
 {
-	string s;
-	cin>>s;
-	if(s.size()==0)
-		cout<<s;
-	else
-	{
-		vector<int> x;
-		for(int i=0;i<s.size();i+=2)
-		{
-			int y=(int)s[i]-48;
-			x.push_back(y);
-		}
-		sort(x.begin(),x.end());
-		for(int i=0;i<x.size();i++)
-		{
-			if(i==x.size()-1)
-				cout<<x[i];
-		else
-			cout<<x[i]<<"+";
+    string s;
+    cin >> s;
+    if(s.size() == 0)
+        cout << s;
+    else {
+        vector<int> x;
+        for(int i = 0; i < s.size(); i += 2) {
+            int y = (int)s[i] - 48;
+            x.push_back(y);
+        }
+        sort(x.begin(), x.end());
+        for(int i = 0; i < x.size(); i++) {
+            if(i == x.size() - 1)
+                cout << x[i];
+            else
+                cout << x[i] << "+";
+        }
+    }
 
-		}
-	}
-
-	return 0;
+    return 0;
 }
